253fde878d8ed94e720e980ec5c7189c2659e550
Eugene <auricamercer@gmail.com>
2024-01-16 19:38:11 +0300

    viewModelTest

---
+++ app/src/main/java/com/malomnogo/quizgame/QuizViewModel.kt
@@ -0,0 +1,18 @@
+package com.malomnogo.quizgame
+
+class QuizViewModel() {
+    fun choose(toString: String): UiState {
+        TODO("Not yet implemented")
+
+    }
+
+    fun next() : UiState {
+        TODO("Not yet implemented")
+    }
+
+    fun init(): UiState {
+        TODO("Not yet implemented")
+    }
+
+
+}
